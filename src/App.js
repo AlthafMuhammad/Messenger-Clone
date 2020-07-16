cf6ff23c19c5ce9c008b29be4d07e1250969c855
*Althaf <althafmuhammad@computer.org>
2020-07-16 23:20:34 +0530

    Added usercards and guestcards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Message from './Message';
 
 
 function App() {
-  const [input,setInput]=useState("");
-  const [messages,setMessages]=useState([]);
+  const [input,setInput]= useState("");
+  const [messages,setMessages]= useState([{username:'',text:''}]);
+  const [username,setUsername]= useState('');
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"))
+  }, []);
 
   const sendMessage=(e)=>{
     e.preventDefault();
-    setMessages([...messages,input]);
+    setMessages([...messages,{username: username, text:input}]);
     setInput('');
   }
 
   return (
     <div className="App">
       <h1>MESSENGER</h1>
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>Enter Message...</InputLabel>
@@ -25,8 +32,9 @@ function App() {
       </form>
 
       {
-        messages.map(messages=>(
-          <p>{messages}</p>
+        messages.map(message=>(
+          <Message username={username} message={message}/>
+          
         ))
       }
     </div>
---
+++ src/Message.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Message.css';
+
+function Message( {message,username} ) {
+    const isUser= username===message.username;
+    return (
+        <div className={isUser?'message message__user':'message'}>
+            <Card >
+                <CardContent className={isUser?'message__userCard' : 'message__guestCard'}>
+                    <Typography color="white" variant="h5" component="h2">
+                        {message.username}: {message.text}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+        
+        
+    )
+}
+
+export default Message;
