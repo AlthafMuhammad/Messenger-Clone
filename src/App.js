1524a07eb6f0690870c5ee9f48b9e9297e74f88a
*Althaf <althafmuhammad@computer.org>
2020-07-16 20:14:00 +0530

    Added message input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Button,
+  
+
+} from 'react-bootstrap';
 
 function App() {
+  const [input,setInput]=useState("");
+  const [messages,setMessages]=useState([]);
+
+  const sendMessage=(e)=>{
+    e.preventDefault();
+    setMessages([...messages,input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
       <h1>MESSENGER</h1>
+      <form>
+        <input value={input} onChange={e=>setInput(e.target.value)}/>
+        <Button type="submit" onClick={sendMessage}>Send Message</Button>
+      </form>
 
-
-
+      {
+        messages.map(messages=>(
+          <p>{messages}</p>
+        ))
+      }
     </div>
   );
 }
