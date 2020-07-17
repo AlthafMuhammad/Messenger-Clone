8dd1a988fa003a8b37ce3f488c0347e4b896fc16
*Althaf <althafmuhammad@computer.org>
2020-07-18 00:34:34 +0530

    added input to bottom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { Button, FormControl, Input, InputLabel, Container, Grid } from '@material-ui/core';
 import Message from './Message';
 import db from './firebase';
 import firebase from 'firebase';
@@ -38,26 +38,27 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"/>
-      <h2>Welcome {username}</h2>
-      <form className="app__form">
-        <FormControl>
-          <InputLabel>Enter Message...</InputLabel>
-          <Input value={input} onChange={e=>setInput(e.target.value)}/>
-          <IconButton disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}><SendIcon/></IconButton>
-        </FormControl>
-      </form>
+      <div className="App">
+        <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"/>
+        <h2>Welcome {username}</h2>
+        <div>
+          <form className="app__form">
+            <FormControl className="app__formControl">
+              <Input className="app__input" placeholder="Enter message..." value={input} onChange={e=>setInput(e.target.value)}/>
+              <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}><SendIcon/></IconButton>
+            </FormControl>
+          </form>
+        </div>
 
-      <FlipMove>
-        {
-          messages.map(({id,message})=>(
-            <Message key={id} username={username} message={message}/>
-            
-          ))
-        }
-      </FlipMove>
-    </div>
+        <FlipMove>
+          {
+            messages.map(({id,message})=>(
+              <Message key={id} username={username} message={message}/>
+              
+            ))
+          }
+        </FlipMove>
+      </div>
   );
 }
 
--- src/Message.js
+++ src/Message.js
@@ -7,7 +7,7 @@ import FlipMove from 'react-flip-move';
 const Message=forwardRef(({message,username},ref) => {
     const isUser= username===message.username;
     return (
-        <div ref={ref} className={isUser?'message message__user':'message'}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card style={{borderRadius:'1.3em',lineHeight:'1.34'}}>
                 <CardContent className={isUser?'message__userCard' : 'message__guestCard'} style={{paddingTop:'6px',paddingRight:'12px',paddingBottom:'7px',paddingLeft:'12px'}}>
                     <Typography>
