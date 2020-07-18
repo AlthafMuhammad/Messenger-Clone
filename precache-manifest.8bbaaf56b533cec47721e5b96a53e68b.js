9e851d0a9c1795124971f98e2b4ba76caf14926b
*Althaf <althafmuhammad@computer.org>
2020-07-18 11:58:14 +0530

    Updates

---
+++ precache-manifest.8bbaaf56b533cec47721e5b96a53e68b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dffe499e8e539a9f1aaf886950a9fc",
+    "url": "/Messenger-Clone/index.html"
+  },
+  {
+    "revision": "6aa1ea8748efc82ea918",
+    "url": "/Messenger-Clone/static/css/main.514061d9.chunk.css"
+  },
+  {
+    "revision": "e6b65c5cbe3f4ee81c16",
+    "url": "/Messenger-Clone/static/js/2.617bfe0d.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Messenger-Clone/static/js/2.617bfe0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa1ea8748efc82ea918",
+    "url": "/Messenger-Clone/static/js/main.b7ccd747.chunk.js"
+  },
+  {
+    "revision": "673bca750518769ff508",
+    "url": "/Messenger-Clone/static/js/runtime-main.95c810cf.js"
+  }
+]);
