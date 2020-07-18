905ee3d451868155ad08afe55b5f2b5e857f280d
*Althaf <althafmuhammad@computer.org>
2020-07-18 10:43:16 +0530

    modified guestname style

--- src/Message.js
+++ src/Message.js
@@ -4,19 +4,27 @@ import './Message.css';
 import FlipMove from 'react-flip-move';
 
 
-const Message=forwardRef(({message,username},ref) => {
+const Message=React.forwardRef(({message,username},ref) => {
     const isUser= username===message.username;
     return (
-        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
-            <Card style={{borderRadius:'1.3em',lineHeight:'1.34'}}>
-                <CardContent className={isUser?'message__userCard' : 'message__guestCard'} style={{paddingTop:'6px',paddingRight:'12px',paddingBottom:'7px',paddingLeft:'12px'}}>
-                    <Typography>
-                        {!isUser && `${message.username || 'Unknown User'}:` } {message.message}
-                        
-                    </Typography>
-                </CardContent>
-            </Card>
-        </div>
+            <div>
+                <div  ref={ref} className={`message ${isUser && 'message__user'}`}>
+                <div ref={ref} className="mess__username">
+                   
+                   {!isUser && `${message.username || 'Unknown User'}:` }
+               
+                </div>
+                    
+                    <Card style={{borderRadius:'1.3em',lineHeight:'1.34'}}>
+                        <CardContent className={isUser?'message__userCard' : 'message__guestCard'} style={{paddingTop:'6px',paddingRight:'12px',paddingBottom:'7px',paddingLeft:'12px'}}>
+                            <Typography>
+                                {message.message}
+                                
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         
         
     )
